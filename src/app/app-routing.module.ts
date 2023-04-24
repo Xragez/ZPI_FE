c285555679953a598c57eb042257fe771627cafe
Ernest <eperzylo123@gmail.com>
2023-04-24 21:28:07 +0200

    user_dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { MainComponent } from './main/main.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { DashboardHomeComponent } from './dashboard-home/dashboard-home.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/main', pathMatch: 'full' },
   {path: 'main', component: MainComponent },
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'user_dashboard', component: UserDashboardComponent,
+    children: [
+      {path: 'home_dashboard', component: DashboardHomeComponent },
+    ]
+  },
+
 ];
 
 @NgModule({
