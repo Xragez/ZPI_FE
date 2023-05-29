bbecc78ce6143ad7f6279b2c43867dbb5b9e1030
Ernest <eperzylo123@gmail.com>
2023-05-29 18:47:14 +0200

    rating.v2

--- src/app/photo-modal/photo-modal.component.ts
+++ src/app/photo-modal/photo-modal.component.ts
@@ -33,10 +33,30 @@ export class PhotoModalComponent implements OnInit{
 
   
   stars = [1,2,3,4,5];
-  rating=3;
+  ratings= [
+    {user_id: 1, rating: 5},
+    {user_id: 2, rating: 4},
+    {user_id: 3, rating: 3},
+    {user_id: 1, rating: 4},
+    {user_id: 1, rating: 5},
+  ]
+  
+  avarageRating(): number{
+    if(this.ratings.length == 0) {
+      return 0;
+    }
+    
+    let totalRating = 0;
+    for(let i=0; i < this.ratings.length; i++){
+      totalRating += this.ratings[i].rating;
+    }
+    const avarage = totalRating / this.ratings.length;
+    return Number(avarage.toFixed(2))
+  }
 
+  rate = 0;
   updateRating(r: any){
-    this.rating = r
-    console.log(this.rating)
+    this.ratings.push({user_id: 3, rating:r})
+    this.rate = r;
   }
 }
